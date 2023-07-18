1af9f9bc334e8d7d1cd4cec394a9125e0321a5e3
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-07-18 11:07:48 +0300

    set up fixes 2

--- src/components/WebBuilder.jsx
+++ src/components/WebBuilder.jsx
@@ -1,44 +1,12 @@
 import React, { useEffect, useRef } from 'react';
-import 'grapesjs/dist/css/grapes.min.css';
 import grapesjs from 'grapesjs';
+import grapeJsConfig from './grapeJsConfig';
 
 function WebBuilder() {
   const editorRef = useRef(null);
 
   useEffect(() => {
-    const editor = grapesjs.init({
-      container: editorRef.current,
-      height: '300px',
-      width: 'auto',
-      storageManager: false,
-      panels: { defaults: [] },
-      blockManager: {
-        appendTo: '#blocks',
-        blocks: [
-          {
-            id: 'section',
-            label: '<b>Section</b>',
-            attributes: { class: 'gjs-block-section' },
-            content: `<section>
-              <h1>This is a simple title</h1>
-              <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
-            </section>`,
-          },
-          {
-            id: 'text',
-            label: 'Text',
-            content: '<div data-gjs-type="text">Insert your text here</div>',
-          },
-          {
-            id: 'image',
-            label: 'Image',
-            select: true,
-            content: { type: 'image' },
-            activate: true,
-          },
-        ],
-      },
-    });
+    const editor = grapesjs.init(grapeJsConfig);
 
     const h1Component = editor.DomComponents.addComponent({
       tagName: 'h1',
@@ -54,8 +22,8 @@ function WebBuilder() {
 
   return (
     <>
-      <div ref={editorRef} />
-      <div id="blocks" />
+      <div id="gjs"></div>
+      <div id="blocks"></div>
     </>
   );
 }
--- src/components/grapeJsConfig.js
+++ src/components/grapeJsConfig.js
@@ -1,31 +1,37 @@
-import React, { useEffect, useRef } from 'react';
-import grapesjs from 'grapesjs';
-import grapeJsConfig from './grapeJsConfig';
+import 'grapesjs/dist/css/grapes.min.css';
 
-function WebBuilder() {
-  const editorRef = useRef(null);
+const grapeJsConfig = {
+  container: '#gjs',
+  height: '500px',
+  width: 'auto',
+  storageManager: false,
+  panels: { defaults: [] },
+  blockManager: {
+    appendTo: '#blocks',
+    blocks: [
+      {
+        id: 'section',
+        label: '<b>Section</b>',
+        attributes: { class: 'gjs-block-section' },
+        content: `<section>
+          <h1>This is a simple title</h1>
+          <div>This is just a Lorem text: Lorem ipsum dolor sit amet</div>
+        </section>`,
+      },
+      {
+        id: 'text',
+        label: 'Text',
+        content: '<div data-gjs-type="text">Insert your text here</div>',
+      },
+      {
+        id: 'image',
+        label: 'Image',
+        select: true,
+        content: { type: 'image' },
+        activate: true,
+      },
+    ],
+  },
+};
 
-  useEffect(() => {
-    const editor = grapesjs.init(grapeJsConfig);
-
-    const h1Component = editor.DomComponents.addComponent({
-      tagName: 'h1',
-      content: 'Hello World Component!',
-    });
-
-    editor.setComponents([h1Component]);
-
-    return () => {
-      editor.destroy();
-    };
-  }, []);
-
-  return (
-    <>
-      <div id="gjs"></div>
-      <div id="blocks"></div>
-    </>
-  );
-}
-
-export default WebBuilder;
+export default grapeJsConfig;
