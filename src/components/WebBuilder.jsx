c1db9d091bec2a15949bc644095edad376dadacf
SHARKZTECH <mosenjemudogoz@gmail.com>
2023-07-18 11:25:19 +0300

    panel and button

--- src/components/WebBuilder.jsx
+++ src/components/WebBuilder.jsx
@@ -3,8 +3,6 @@ import grapesjs from 'grapesjs';
 import grapeJsConfig from './grapeJsConfig';
 
 function WebBuilder() {
-  const editorRef = useRef(null);
-
   useEffect(() => {
     const editor = grapesjs.init(grapeJsConfig);
 
@@ -15,6 +13,45 @@ function WebBuilder() {
 
     editor.setComponents([h1Component]);
 
+    editor.Panels.addPanel({
+      id: 'panel-top',
+      el: '.panel__top',
+    });
+
+    editor.Panels.addPanel({
+      id: 'basic-actions',
+      el: '.panel__basic-actions',
+      buttons: [
+        {
+          id: 'visibility',
+          active: true,
+          className: 'btn-toggle-borders',
+          label: '<u>B</u>',
+          command: 'sw-visibility',
+        },
+        {
+          id: 'export',
+          className: 'btn-open-export',
+          label: 'Exp',
+          command: 'export-template',
+          context: 'export-template',
+        },
+        {
+          id: 'show-json',
+          className: 'btn-show-json',
+          label: 'JSON',
+          context: 'show-json',
+          command(editor) {
+            editor.Modal.setTitle('Components JSON')
+              .setContent(`<textarea style="width:100%; height: 250px;">
+                ${JSON.stringify(editor.getComponents())}
+              </textarea>`)
+              .open();
+          },
+        },
+      ],
+    });
+
     return () => {
       editor.destroy();
     };
@@ -22,6 +59,9 @@ function WebBuilder() {
 
   return (
     <>
+      <div className="panel__top">
+        <div className="panel__basic-actions"></div>
+      </div>
       <div id="gjs"></div>
       <div id="blocks"></div>
     </>
